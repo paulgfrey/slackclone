3b701bd42ff5f3c66f423c81ce4e55a48c0e710c
Arun <kumar_sundaram82@yahoo.com>
2016-08-11 15:48:58 -0400

    second one

--- CreateDB.js
+++ CreateDB.js
@@ -17,8 +17,8 @@ var db = new sqlite3.Database('slackclone.db');
 if (!dbexists) {
     db.serialize(function() {
         var createTEAMTableSql = "CREATE TABLE IF NOT EXISTS TEAM " +
-                       "(ID             INTEGER PRIMARY KEY AUTOINCREMENT   NOT NULL," +
-                       " NAME           CHAR(50)    UNIQUE CONSTRAINT     NOT NULL)"; 
+                       "(ID             INTEGER PRIMARY KEY AUTOINCREMENT," +
+                       " NAME           CHAR(50)    NOT NULL UNIQUE )"; 
 
 
         var createUSERSTableSql = "CREATE TABLE IF NOT EXISTS USERS " +
@@ -30,24 +30,29 @@ if (!dbexists) {
 
         var createTEAMUSERSTableSql = "CREATE TABLE IF NOT EXISTS TEAMUSERS " +
                     "( ID         INTEGER PRIMARY KEY AUTOINCREMENT    NOT NULL," +
+                     " USERID     INTEGER ," +
+                    " TEAMID      INTEGER ," +
                     " FOREIGN KEY(USERID) REFERENCES USERS(ID), " + 
-                    " FOREIGN KEY(TEAMID) REFERENCES TEAM(ID) " ;
+                    " FOREIGN KEY(TEAMID) REFERENCES TEAM(ID) ) " ;
 
 
         var createCHANNELTableSql = "CREATE TABLE IF NOT EXISTS CHANNEL " +
                     "(ID        INTEGER PRIMARY KEY AUTOINCREMENT   NOT NULL," +
-                    " NAME      CHAR(25)   NOT NULL  ,"  +
-                    " FOREIGN KEY(TEAMID) REFERENCES TEAM(ID) ,"  +
-                    " DESCRIPTION  CHAR(50) , "   +
-                    " TYPE      CHAR(10)   NOT NULL ) " ;
+                    " NAME      CHAR(25)   NOT NULL  ,"  + 
+                     "TEAMID  INTEGER ," +
+                     " DESCRIPTION  CHAR(50) , "   +
+                    " TYPE      CHAR(10)   NOT NULL , " +
+                     " FOREIGN KEY(TEAMID) REFERENCES TEAM(ID) )"  ;
                     
 
        var createMESSAGESTableSql = "CREATE TABLE IF NOT EXISTS MESSAGES " +
                     "(ID        INTEGER PRIMARY KEY AUTOINCREMENT   NOT NULL ," +
-                    " NAME      CHAR(25)   NOT NULL ,"  +
-                    "FOREIGN KEY(USERID) REFERENCES USERS(ID), " +  
-                    "FOREIGN KEY(CHANNELID) REFERENCES CHANNEL(ID), " +  
-                    "TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP )" ; 
+                    " NAME      CHAR(25)   NOT NULL ,"  + 
+                    " USERID     INTEGER ," +
+                    " CHANNELID     INTEGER ," +
+                    " TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP ," + 
+                      "FOREIGN KEY(USERID) REFERENCES USERS(ID), " +   
+                       "FOREIGN KEY(CHANNELID) REFERENCES CHANNEL(ID)) "   ;
 
 
 
@@ -57,7 +62,10 @@ if (!dbexists) {
         db.run(createCHANNELTableSql);
         db.run(createMESSAGESTableSql);
 
+
         });
+
+        // db.exec("COMMIT");
     }
 
  
--- CreateTestDB.js
+++ CreateTestDB.js
@@ -16,9 +16,9 @@ var db = new sqlite3.Database('testslackclone.db');
 
 if (!dbexists) {
     db.serialize(function() {
-        var createTEAMTableSql = "CREATE TABLE IF NOT EXISTS TEAM " +
-                       "(ID             INTEGER PRIMARY KEY AUTOINCREMENT   NOT NULL," +
-                       " NAME           CHAR(50)    UNIQUE CONSTRAINT     NOT NULL)"; 
+         var createTEAMTableSql = "CREATE TABLE IF NOT EXISTS TEAM " +
+                       "(ID             INTEGER PRIMARY KEY AUTOINCREMENT," +
+                       " NAME           CHAR(50)    NOT NULL UNIQUE )"; 
 
 
         var createUSERSTableSql = "CREATE TABLE IF NOT EXISTS USERS " +
@@ -30,25 +30,29 @@ if (!dbexists) {
 
         var createTEAMUSERSTableSql = "CREATE TABLE IF NOT EXISTS TEAMUSERS " +
                     "( ID         INTEGER PRIMARY KEY AUTOINCREMENT    NOT NULL," +
+                     " USERID     INTEGER ," +
+                    " TEAMID      INTEGER ," +
                     " FOREIGN KEY(USERID) REFERENCES USERS(ID), " + 
-                    " FOREIGN KEY(TEAMID) REFERENCES TEAM(ID) " ;
+                    " FOREIGN KEY(TEAMID) REFERENCES TEAM(ID) ) " ;
 
 
         var createCHANNELTableSql = "CREATE TABLE IF NOT EXISTS CHANNEL " +
                     "(ID        INTEGER PRIMARY KEY AUTOINCREMENT   NOT NULL," +
-                    " NAME      CHAR(25)   NOT NULL  ,"  +
-                    " FOREIGN KEY(TEAMID) REFERENCES TEAM(ID) ,"  +
-                    " DESCRIPTION  CHAR(50) , "   +
-                    " TYPE      CHAR(10)   NOT NULL ) " ;
+                    " NAME      CHAR(25)   NOT NULL  ,"  + 
+                     "TEAMID  INTEGER ," +
+                     " DESCRIPTION  CHAR(50) , "   +
+                    " TYPE      CHAR(10)   NOT NULL , " +
+                     " FOREIGN KEY(TEAMID) REFERENCES TEAM(ID) )"  ;
                     
 
        var createMESSAGESTableSql = "CREATE TABLE IF NOT EXISTS MESSAGES " +
                     "(ID        INTEGER PRIMARY KEY AUTOINCREMENT   NOT NULL ," +
-                    " NAME      CHAR(25)   NOT NULL ,"  +
-                    "FOREIGN KEY(USERID) REFERENCES USERS(ID), " +  
-                    "FOREIGN KEY(CHANNELID) REFERENCES CHANNEL(ID), " +  
-                    "TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP )" ; 
-
+                    " NAME      CHAR(25)   NOT NULL ,"  + 
+                    " USERID     INTEGER ," +
+                    " CHANNELID     INTEGER ," +
+                    " TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP ," + 
+                      "FOREIGN KEY(USERID) REFERENCES USERS(ID), " +   
+                       "FOREIGN KEY(CHANNELID) REFERENCES CHANNEL(ID)) "   ;
 
 
         db.run(createTEAMTableSql);
@@ -57,10 +61,39 @@ if (!dbexists) {
         db.run(createCHANNELTableSql);
         db.run(createMESSAGESTableSql);
 
+        var insertTEAMTableSql = "INSERT INTO TEAM ( NAME) " +
+            "VALUES ('team1')," +
+                   "('team2')," +
+                   "('team3')" ;
+
+        var insertUSERSTableSql = "INSERT INTO USERS (NAME, PASSWORD, EMAIL) " +
+           "VALUES ('shuvo', 'QWEWRER', 'BLASHS@GMAIL.COM')," +
+                   "('GREG', 'QWEWRER',  'RTERE@GMAIL.COM')," +
+                   "('PAUL', 'QWEWRER', 'BRRTTS@GMAIL.COM')," +
+                   "( 'JASON', 'QWEWRER', 'BRTRRTS@GMAIL.COM')" ;
+
+                
+
+        // var insertTEAMUSERSTableSql = "INSERT INTO TWEET (USERID, TWEET, DATE) " +
+        //      "VALUES ('shuvo',      'Welcome to Tweeter Clone',                     '2016-08-05 12:45:00'), " +
+        //             "('abu',        'Tweet by Abu',                                 '2016-08-05 12:46:00'), " +
+         
+      
+        db.run(insertTEAMTableSql);
+        db.run(insertUSERSTableSql);
+
+     //   db.run(insertTEAMUSERSTableSql);
+
+   
+    });
+
+}
+
+
+ db.each("SELECT * FROM TEAM", function(err, row) {
+            console.log(row.ID + ": " + row.NAME);
         });
-    }
 
- 
-
+        
 // db.close();
 
