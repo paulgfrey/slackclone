40afc4cd0832083802f70f67d70af24aa4919ad8
bscam3ay <bhavin_ny@yahoo.com>
2016-08-19 13:46:29 -0400

    added channels controller

--- webapp/channelsCtrl.js
+++ webapp/channelsCtrl.js
@@ -1,3 +1,8 @@
 slackCloneApp.controller('channelsCtrl', function ($rootScope, $scope, $location, service) {
   console.log('channelsCtrl');
+service.getChannels($rootScope.team.Id,$rootScope.user.id)
+  .then((channels) => {
+    $scope.channels = channels;
+  });
+
 });
