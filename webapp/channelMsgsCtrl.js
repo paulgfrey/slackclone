3fda9c2652973e04ff75ab23e9648ec257005cc6
paulgfrey <paulgfrey@gmail.com>
2016-08-22 13:00:01 -0400

    fixed channelMsgCtrl to not refresh abruptly

--- webapp/channelMsgsCtrl.js
+++ webapp/channelMsgsCtrl.js
@@ -1,28 +1,29 @@
-slackCloneApp.controller('channelMsgsCtrl', function ($rootScope, $scope, $location, $timeout, $route, service) {
-  console.log('channelMsgsCtrl');
-  $scope.messages = [];
-  service.getMsgs($rootScope.channel.id)
-    .then((messages) => {
-      //var tempMessageList = []; 
-      for (var i = 0; i < messages.length; i++) {
-        console.log("Message's User ID: " + messages[i].userId);
-        console.log("Message's content: " + messages[i].message);
-        messages[i].timeStamp = new Date(messages[i].timeStamp);
-        (function () {
-          var j = i;
-          service.getUser(messages[j].userId).then((retrievedUserID) => {
-            console.log("Retrieved User Name is: " + retrievedUserID.name);
-            $scope.messages.push({
-              timeStamp: messages[j].timeStamp,
-              userName: retrievedUserID.name,
-              message: messages[j].message
+slackCloneApp.controller('channelMsgsCtrl', function ($rootScope, $scope, $location, $timeout, service) {
+  $scope.updateMessagePanel = function () {
+    service.getMsgs($rootScope.channel.id)
+      .then((messages) => {
+        //var tempMessageList = []; 
+        for (var i = 0; i < messages.length; i++) {
+          console.log("Message's User ID: " + messages[i].userId);
+          console.log("Message's content: " + messages[i].message);
+          messages[i].timeStamp = new Date(messages[i].timeStamp);
+          (function () {
+            var j = i;
+            service.getUser(messages[j].userId).then((retrievedUserID) => {
+              console.log("Retrieved User Name is: " + retrievedUserID.name);
+              $scope.messages.push({
+                timeStamp: messages[j].timeStamp,
+                userName: retrievedUserID.name,
+                message: messages[j].message
+              });
+              $scope.$apply();
             });
-            $scope.$apply();
-          });
-        })();
+          })();
 
-      }
-    });
+        }
+      });
+  }
+
   $scope.postMessage = function (newMessage) {
     console.log("Enterting Post Message: " + newMessage);
     var channelId = $rootScope.channel.id;
@@ -30,12 +31,13 @@ slackCloneApp.controller('channelMsgsCtrl', function ($rootScope, $scope, $locat
     service.postMsg(channelId, userId, newMessage, function () {
       console.log("In call back");
       $timeout(function () {
-        // Any code in here will automatically have an $scope.apply() run afterwards
         console.log("in Timeout callback");
-        $route.reload();
-        // And it just works!
+        $scope.updateMessagePanel();
       });
     });
   }
+  console.log('channelMsgsCtrl');
 
+  $scope.messages = [];
+  $scope.updateMessagePanel();
 });
