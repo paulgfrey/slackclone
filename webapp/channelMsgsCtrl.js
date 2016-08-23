07f58d498993f6f7afd75702f49ec03a243afddb
paulgfrey <paulgfrey@gmail.com>
2016-08-23 10:16:12 -0400

    fixed flash

--- webapp/channelMsgsCtrl.js
+++ webapp/channelMsgsCtrl.js
@@ -1,28 +1,39 @@
-slackCloneApp.controller('channelMsgsCtrl', function ($rootScope, $scope, $location, $timeout, service) {
+slackCloneApp.controller('channelMsgsCtrl', function ($rootScope, $scope, $location, $timeout, $interval, service) {
   $scope.glued = true;
+
   $scope.updateMessagePanel = function () {
     service.getMsgs($rootScope.channel.id)
       .then((messages) => {
-        $scope.messages = [];
-        //var tempMessageList = []; 
-        $scope.messages = [];
+        if (!$scope.messages) {
+          $scope.messages = [];
+        }
+        //$scope.messages = [];
         for (var i = 0; i < messages.length; i++) {
-          console.log("Message's User ID: " + messages[i].userId);
-          console.log("Message's content: " + messages[i].message);
-          messages[i].timeStamp = new Date(messages[i].timeStamp);
-          (function () {
-            var j = i;
-            service.getUser(messages[j].userId).then((retrievedUserID) => {
-              console.log("Retrieved User Name is: " + retrievedUserID.name);
-              $scope.messages.push({
-                timeStamp: messages[j].timeStamp,
-                userName: retrievedUserID.name,
-                message: messages[j].message
+          var msgFound = false;
+          for (var x = 0; x < $scope.messages.length && !msgFound; x++) {
+            if (messages[i].id === $scope.messages[x].id) {
+              msgFound = true;
+            }
+          }
+          if (! msgFound) {
+            console.log("Message's User ID: " + messages[i].userId);
+            console.log("Message's content: " + messages[i].message);
+            messages[i].timeStamp = new Date(messages[i].timeStamp);
+            (function () {
+              var j = i;
+              service.getUser(messages[j].userId).then((retrievedUserID) => {
+                console.log("Retrieved User Name is: " + retrievedUserID.name);
+                $scope.messages.push({
+                  id: messages[j].id,
+                  timeStamp: messages[j].timeStamp,
+                  userName: retrievedUserID.name,
+                  message: messages[j].message,
+                  displayed: true
+                });
+                $scope.$apply();
               });
-              $scope.$apply();
-            });
-          })();
-
+            })();
+          }
         }
         $scope.$apply();
       });
@@ -44,10 +55,20 @@ slackCloneApp.controller('channelMsgsCtrl', function ($rootScope, $scope, $locat
   }
   console.log('channelMsgsCtrl');
 
-  $scope.messages = [];
   $scope.updateMessagePanel();
-  $scope.$on('ChannelChanged', function() {
+  $scope.$on('ChannelChanged', function () {
+    $scope.messages = [];
     $scope.updateMessagePanel();
   });
 
+  if (!$rootScope.msgTimer) {
+    $rootScope.msgTimer = function () {
+      //Initialize the Timer to run every 1000 milliseconds i.e. one second.
+      console.log('Initializing the Timer to run every second.');
+      $scope.timer = $interval(function () {
+        $scope.updateMessagePanel();
+      }, 1000);
+    };
+    $rootScope.msgTimer();
+  }
 });
